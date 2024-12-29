0363e4051ec3eab9016f8f218608f9420998863f
Muhammad Mehdi <muhammadmahdi512@gmail.com>
2024-12-29 23:08:13 +0500

    Brand Navigations Fixed !

--- app/(brand)/brand/_components/Sidebar/Navigation.tsx
+++ app/(brand)/brand/_components/Sidebar/Navigation.tsx
@@ -2,7 +2,7 @@
 
 import { usePathname } from "next/navigation";
 import {
-  Fullscreen,
+  // Fullscreen,
   Users,
   UserCircle,
   Store,
@@ -24,67 +24,66 @@ export const Navigation = () => {
   // Define routes conditionally based on the path
   const routes =
     storeId &&
-      pathname.includes(`/admin/brand/${brandId}/admin-store/${storeId}`)
+    pathname.includes(`/admin/brand/${brandId}/admin-store/${storeId}`)
       ? [
-        {
-          label: "Store Dashboard",
-          href: `/admin/brand/${brandId}/admin-store/${storeId}`,
-          icon: Store,
-        },
-        {
-          label: "Store Flyers",
-          href: `/admin/brand/${brandId}/admin-store/${storeId}/flyers`,
-          icon: UserCircle,
-        },
-        {
-          label: "Store Special Events",
-          href: `/admin/brand/${brandId}/admin-store/${storeId}/special-events`,
-          icon: Users,
-        },
-        {
-          label: "Store Coupon Events",
-          href: `/admin/brand/${brandId}/admin-store/${storeId}/coupon-gifts`,
-          icon: Users,
-        },
-        {
-          label: "Store Settings",
-          href: `/admin/brand/${brandId}/admin-store/${storeId}/settings`,
-          icon: Settings,
-        },
-      ]
+          {
+            label: "Store Dashboard",
+            href: `/admin/brand/${brandId}/admin-store/${storeId}`,
+            icon: Store,
+          },
+          {
+            label: "Store Flyers",
+            href: `/admin/brand/${brandId}/admin-store/${storeId}/flyers`,
+            icon: UserCircle,
+          },
+          {
+            label: "Store Special Events",
+            href: `/admin/brand/${brandId}/admin-store/${storeId}/special-events`,
+            icon: Users,
+          },
+          {
+            label: "Store Coupon Events",
+            href: `/admin/brand/${brandId}/admin-store/${storeId}/coupon-gifts`,
+            icon: Users,
+          },
+          {
+            label: "Store Settings",
+            href: `/admin/brand/${brandId}/admin-store/${storeId}/settings`,
+            icon: Settings,
+          },
+        ]
       : [
-        {
-          label: "Home",
-          href: `/`,
-          icon: Fullscreen,
-        },
-        {
-          label: "Brand Stores",
-          href: `/brand/${brandId}/stores`,
-          icon: UserCircle,
-        },
-        {
-          label: "Brand Flyers",
-          href: `/brand/${brandId}/flyers`,
-          icon: UserCircle,
-        },
-        {
-          label: "Brand Special Events",
-          href: `/brand/${brandId}/special-events`,
-          icon: UserCircle,
-        },
-        {
-          label: "Brand Coupon Gifts",
-          href: `/brand/${brandId}/coupon-gifts`,
-          icon: Gift,
-        },
-        {
-          label: "Settings",
-          href: `/brand/${brandId}/settings`,
-          icon: Users,
-        },
-
-      ];
+          // {
+          //   label: "Home",
+          //   href: `/`,
+          //   icon: Fullscreen,
+          // },
+          {
+            label: "Brand Stores",
+            href: `/brand/${brandId}/stores`,
+            icon: UserCircle,
+          },
+          {
+            label: "Brand Flyers",
+            href: `/brand/${brandId}/flyers`,
+            icon: UserCircle,
+          },
+          {
+            label: "Brand Special Events",
+            href: `/brand/${brandId}/special-events`,
+            icon: UserCircle,
+          },
+          {
+            label: "Brand Coupon Gifts",
+            href: `/brand/${brandId}/coupon-gifts`,
+            icon: Gift,
+          },
+          {
+            label: "Settings",
+            href: `/brand/${brandId}/settings`,
+            icon: Users,
+          },
+        ];
 
   // Show skeletons while user data is loading
   if (!user?.name) {
