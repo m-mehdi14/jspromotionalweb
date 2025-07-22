94e700ea676142df1bd75ee311a206ce11d9fc19
Muhammad Mehdi <muhammadmahdi512@gmail.com>
2025-07-22 17:35:02 +0500

    Update Firebase configuration with new project details

--- config/firebase.ts
+++ config/firebase.ts
@@ -49,14 +49,23 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCrP5kjUBR9VDrOYBKNhOBRe70pzyYyjO0",
+//   authDomain: "jspromotionalapp.firebaseapp.com",
+//   projectId: "jspromotionalapp",
+//   storageBucket: "jspromotionalapp.firebasestorage.app",
+//   messagingSenderId: "383720093577",
+//   appId: "1:383720093577:web:fc8dbb1ab3ba43edd8d358",
+//   measurementId: "G-L6NNW5L86C",
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCrP5kjUBR9VDrOYBKNhOBRe70pzyYyjO0",
-  authDomain: "jspromotionalapp.firebaseapp.com",
-  projectId: "jspromotionalapp",
-  storageBucket: "jspromotionalapp.firebasestorage.app",
-  messagingSenderId: "383720093577",
-  appId: "1:383720093577:web:fc8dbb1ab3ba43edd8d358",
-  measurementId: "G-L6NNW5L86C",
+  apiKey: "AIzaSyDkTByojrnDP0zNFV5mVPkdBY0KhgTNGwM",
+  authDomain: "easyfllyer.firebaseapp.com",
+  projectId: "easyfllyer",
+  storageBucket: "easyfllyer.firebasestorage.app",
+  messagingSenderId: "63894906325",
+  appId: "1:63894906325:web:6279a8851f777863cf895c",
 };
 
 // Initialize Firebase
